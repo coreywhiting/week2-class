3a7d35b2b821363d7042118726e938da8eb0d306
Jeff Cohen <cohen.jeff@gmail.com>
2015-06-30 19:10:16 -0500

    One way to implement a "featured product of the day"

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,8 @@ class ProductsController < ApplicationController
     render "index"
   end
 
+  def featured
+    render 'featured'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   get("/catalog", :controller => 'products', :action => 'index')
 
+  get("/featured", :controller => 'products', :action => 'featured')
 end
